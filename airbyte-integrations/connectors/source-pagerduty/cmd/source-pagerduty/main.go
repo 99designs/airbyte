8e5ff7e172387fcb76995dde67ed48022d14786f
Eamon Taaffe <eamon@taaffe.com.au>
2021-10-25 09:40:53 +1100

    WIP: Stared working on the spec command

--- airbyte-integrations/connectors/source-pagerduty/cmd/source-pagerduty/main.go
+++ airbyte-integrations/connectors/source-pagerduty/cmd/source-pagerduty/main.go
@@ -1,26 +1,37 @@
 package main
 
 import (
-	"os"
-	"log"
+	_ "embed"
+	"encoding/json"
 	"github.com/urfave/cli/v2"
+	"internal"
+	"log"
+	"os"
 )
 
 func main() {
 	app := &cli.App{
-    Name: "source-pagerduty",
-    Usage: "An Airbyte Source for the PagerDuty API.",
-    Commands: []*cli.Command{
+		Name:  "source-pagerduty",
+		Usage: "An Airbyte Source for the PagerDuty API.",
+		Commands: []*cli.Command{
 			{
-				Name: "spec",
+				Name:  "spec",
 				Usage: "Returns the connector specification.",
 				Action: func(c *cli.Context) error {
-					log.Println("Hello, spec!")
+					err, spec := Specification()
+
+					if err != nil {
+						panic(err)
+					}
+
+					specBytes, err := json.Marshall(spec)
+					log.Println(string(specBytes))
+
 					return nil
 				},
 			},
 			{
-				Name: "check",
+				Name:  "check",
 				Usage: "The check command attempts to connect to the API.",
 				Action: func(c *cli.Context) error {
 					log.Println("Hello, check!")
@@ -28,7 +39,7 @@ func main() {
 				},
 			},
 			{
-				Name: "discover",
+				Name:  "discover",
 				Usage: "This command detects the structure of the data.",
 				Action: func(c *cli.Context) error {
 					log.Println("Hello, discover!")
@@ -36,7 +47,7 @@ func main() {
 				},
 			},
 			{
-				Name: "read",
+				Name:  "read",
 				Usage: "This command reads data from the underlying data source.",
 				Action: func(c *cli.Context) error {
 					log.Println("Hello, read!")
@@ -44,10 +55,10 @@ func main() {
 				},
 			},
 		},
-  }
+	}
 
-  err := app.Run(os.Args)
-  if err != nil {
-    log.Fatal(err)
-  }
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- airbyte-integrations/connectors/source-pagerduty/go.mod
+++ airbyte-integrations/connectors/source-pagerduty/go.mod
@@ -3,7 +3,9 @@ module github.com/airbytehq/airbyte-integrations/connectors/source-pagerduty
 go 1.17
 
 require (
+	github.com/PagerDuty/go-pagerduty v1.4.2 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.0.1 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	github.com/urfave/cli v1.22.5 // indirect
