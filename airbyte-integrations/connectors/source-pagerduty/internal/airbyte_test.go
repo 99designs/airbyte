538f782e93b6772b9ec0f85ba56ed935862b2c1f
Eamon Taaffe <eamon@taaffe.com.au>
2021-10-25 12:07:32 +1100

    Added status enum with encoding

--- airbyte-integrations/connectors/source-pagerduty/internal/airbyte_test.go
+++ airbyte-integrations/connectors/source-pagerduty/internal/airbyte_test.go
@@ -1,14 +1,42 @@
 package internal
 
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
+	"testing"
+	"encoding/json"
 )
 
 func TestSpecification(t *testing.T) {
 	spec, err := Specification()
 
 	assert.Nil(t, err, "Shouldn't return an error")
-	assert.NotZero(t, spec.DocumentationUrl)
-	assert.NotZero(t, spec.ConnectionSpecification)
+	assert.NotZero(t, spec.DocumentationUrl, "Always provide documentation")
+	assert.NotZero(
+		t,
+		spec.ConnectionSpecification,
+		"Always provide specificification",
+	)
 }
+
+func TestStatus(t *testing.T) {
+	blob := `["succeeded","failed"]`
+	statusList := []Status{Succeeded, Failed}
+
+	var unmarshalResult []Status
+	err := json.Unmarshal([]byte(blob), &unmarshalResult)
+
+	assert.Nil(t, err, "Should be a valid json string")
+	assert.Equal(t, statusList, unmarshalResult)
+
+	marshalResult, err := json.Marshal([]Status{Succeeded, Failed})
+
+	assert.Nil(t, err, "Should be able to unmarshal")
+	assert.Equal(t, []byte(blob), marshalResult)
+}
+
+// func TestCheck(t *testing.T) {
+// 	config := Config()
+// 	status, err := Check(config)
+
+// 	assert.Nil(t, err, "Shouldn't return an error")
+// }
